e879d83905b24549bd2556bc039f68e4d6a92d84
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2023-11-26 00:19:32 +0300

    Download directly to current folder

--- yt_dlp_wrapper.py
+++ yt_dlp_wrapper.py
@@ -1,9 +1,26 @@
+import contextlib
+import shutil
 import subprocess
 import sys
 from pathlib import Path
+from typing import Generator
 
 
 DOCKER_IMAGE = 'yt-dlp-ffmpeg'
+
+
+@contextlib.contextmanager
+def download_folder(folder_name: str) -> Generator[Path, None, None]:
+    path = Path(folder_name)
+    path.mkdir(exist_ok=True)
+    try:
+        yield path
+        files = path.glob('*')
+        for file in files:
+            destination = path.parent / file.name
+            shutil.move(file, destination)
+    finally:
+        shutil.rmtree(path)
 
 
 def main() -> int:
@@ -11,16 +28,16 @@ def main() -> int:
         cmd = ('yt-dlp', *sys.argv[1:])
         return subprocess.run(cmd).returncode
 
-    yt_dlp_dir = Path('./yt_dlp_dir')
-    yt_dlp_dir.mkdir(exist_ok=True)
-    cmd = (
-        'docker', 'run', '--rm',
-        '-v', fr'./{yt_dlp_dir.absolute().as_posix()}:/app',
-        DOCKER_IMAGE, *sys.argv[2:]
-    )
-    print(cmd)
+    with download_folder('./yt_dlp_dir') as yt_dlp_dir:
+        yt_dlp_dir.mkdir(exist_ok=True)
+        cmd = (
+            'docker', 'run', '--rm',
+            '-v', f'./{yt_dlp_dir.as_posix()}:/app',
+            DOCKER_IMAGE, *sys.argv[2:]
+        )
 
-    p = subprocess.run(cmd)
+        p = subprocess.Popen(cmd)
+        p.communicate()
     if p.returncode:
         print()
         print('=' * 79)
